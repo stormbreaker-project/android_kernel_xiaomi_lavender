1c39e42fe194c4e7d1744bb1ef9604d459d7d933
dccjd3 <dvbhg563@outlook.com>
2024-06-25 15:22:53 +0800

    Update exec.c

--- fs/exec.c
+++ fs/exec.c
@@ -1531,10 +1531,8 @@ static int exec_binprm(struct linux_binprm *bprm)
  
 #ifdef CONFIG_KSU
 extern bool ksu_execveat_hook __read_mostly;
-extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
-			void *envp, int *flags);
-extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
-				 void *argv, void *envp, int *flags);
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv, void *envp, int *flags);
+extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr, void *argv, void *envp, int *flags);
 #endif
 
 static int do_execveat_common(int fd, struct filename *filename,
